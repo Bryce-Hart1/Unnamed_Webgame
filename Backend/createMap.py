19bba4ea8e4bde7ac758dee17d74e5a3ae969960
Bryce Hart <bryce.hart23@gmail.com>
2026-02-10 14:33:25 -0500

    Added more notes to help understand what work still needs done at this stage.

--- Backend/createMap.py
+++ Backend/createMap.py
@@ -2,10 +2,15 @@ import random
 
 
 
-class ore:
-    def __init__(self, hasIron, hasGold):
-        self.Iron = hasIron
+class resource:
+    def __init__(self, hasMetal, hasWood, hasGold, hasOil, hasSilcon, hasUranium):
+        self.Iron = hasMetal
         self.Gold = hasGold
+        self.Wood = hasWood
+        self.Oil = hasOil
+        self.Silcon = hasSilcon
+        self.Uranium = hasUranium
+
 
 class chunk:
     def __init__(self, size, ore, type):
@@ -14,41 +19,38 @@ class chunk:
         self.type = type
 
         
-#returns a random precent 
-def getRandomPrecent():
-    return random(0, 10)
+# returns a random precent. For example
+# if you want a 50% chance of an event: precent = 50
+def getRandomChance(precent):
+    roll = random(1,100)
+    return (roll < precent)
 
 
-def pickOres(onWater):
-    return ore(True, True)
+def pickResource(onWater):
+    #currently not finished. We need a way to determine (randomly generated or not) what resources a spot should generate.
+    #for example, if we have a water spot, we should not be able to place trees or ores for example, only oil, because this is the only one that makes
+    #sense in this spot. Another example is if we have a tree farm, we should not be able to also have metals on this spot. This is still up for discussion,
+    #but I dont think we should be able to have multiple mines on one chunk, my thinking its its one of the three (not multiple for a chunk) - Tree OR Oil OR
+    # ores, and for water, it should be just oil, but maybe at a higher rate, but we can add that later
+    return resource(True, True, True, True, True, True) #placeholder, but you see how this class should return
 
 
+#returns a chunk object. For now we could have it hard set for some sort of pattern, but this should generate EVERYTHING relating to the chunk, including type,
+# whats underneath, what the surface looks like, etc. I think we should build and flush out this function, but leave the more random generation parts out of it 
+# for now. Right now it should just return a chunk object for the map
 def generateChunk(remaining):
-    #chunkOption = {'s', 'm', 'l', 'c', 'w'}
-    #chances = random(1,5)
-    #if chances > 2:
+    
 
-    ores = pickOres(False)
+    ores = pickResource(False)
     returnChunk = chunk('r',ores, "Desert") #format
 
         
-        
-
-#generate chunk order and value (s,m,l,c,w)
-# s - small island
-# m - med island
-# l - large
-# c - connected piece
-# w - water
+#fill a list (Like a vector) with all of the chunks for this map. So say our map is 8 x 8, we have 64 chunks to generate.) Then when we go to print on the
+# frontend, we can just take the width sqrt(TOTAL_CHUNKS) and know what the map would be and how to format it. This is considering the map is square,
+# which i think it should be. lmk if you have questions
 def generateMap():
     myMap = []
-    for i in range(25):
+    TOTAL_CHUNKS = 64
+    for chunk in TOTAL_CHUNKS:
         myMap.append(generateChunk)
     return myMap
-
-
-#generate chunk (25x)
-
-
-
-
