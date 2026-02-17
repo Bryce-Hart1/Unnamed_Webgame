8968a8d8ea24507e19693660e787a0268fb4b14e
Bryce Hart <bryce.hart23@gmail.com>
2026-02-17 15:26:42 -0500

    added some more samples of ideas explained in ideas.md

--- Frontend/homepage.js
+++ Frontend/homepage.js
@@ -1,15 +1,59 @@
 //this file is for animations and javascript needed for the index.html (main splash page)
 
 const homePageCreateGameButton =  document.getElementById('homePage_creategame_button');
+const homePageJoinGameButtom = document.getElementById('homePage_joingame_button');
+const homePageSelectColorLeft = document.getElementById('homePage_Colorleft');
+const homePageSelectColorRight = document.getElementById('homePage_ColorRight');
+const colorArray = ["red", "orange", "yellow", "green", "blue", "purple"];
+const topArray = ["top1", "top2", "top3", "top4", "top5"];
+const bottomArray = ["bottom1", "bottom2", "bottom3", "bottom4", "bottom5"];
 
 
-homePageCreateGameButton.addEventListener('click', function() {requestedGameStart()}) //if homepage start button is pressed call gamestart
+
+class usersCustomization {
+    constructor() {
+        this.color = "red";
+        this.colorID = 0;
+        this.top = "topZero";
+        this.topID = 0;
+        this.bottom = "BottomZero";
+        this.bottomID = 0;
+    }
+
+}
+
+
+currentUser = new usersCustomization();
+
+
+homePageCreateGameButton.addEventListener('click', function() {requestedGameStart()}); //if homepage start button is pressed call gamestart
+homePageSelectColorLeft.addEventListener('click', function() { currentUser.color = getNewCustomizationInWheel(colorArray, this.colorID, false); });
+homePageSelectColorRight.addEventListener('click', function(){currentUser.color = getNewCustomizationInWheel(colorArray, this.colorID, true)});
 
 
 
 
 
 
+
+
+
+
+
+//takes in spot in array and direction and returns a string of the next item wanted, will do tops, bottoms and colors
+function getNewCustomizationInWheel(Array, currentID, isRight){
+    if(isRight){
+        if(currentID == (Array.length()-1)){
+            return Array[0];
+        }
+        return Array[currentID + 1];
+    }else{ // goes left
+        if(currentColorID == 0){
+            return Array[Array.length()-1]; //return last element
+        }
+        return Array[currentID - 1];
+    }
+}
 
 
 
