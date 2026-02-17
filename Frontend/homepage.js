516b8474382ff2cffe1ae2d5d3b053b0d14b12c5
Bryce Hart <bryce.hart23@gmail.com>
2026-02-17 11:51:19 -0500

    Added some functions to show how we will call the backend from the frontend

--- Frontend/homepage.js
+++ Frontend/homepage.js
@@ -1,1 +1,35 @@
 //this file is for animations and javascript needed for the index.html (main splash page)
+
+const homePageCreateGameButton =  document.getElementById('homePage_creategame_button');
+
+
+homePageCreateGameButton.addEventListener('click', function() {requestedGameStart()}) //if homepage start button is pressed call gamestart
+
+
+
+
+
+
+
+
+
+
+
+async function requestedGameStart(){
+    const url = "WhateverTheFastAPIurlIs";
+    try{
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new error(`http error. status: ${response.status}` );
+        }
+        const data = await response.json();
+        console.log(data);
+        document.getElementById('response-message').innerText = `Message: ${data.message}, Data: ${data.data}`;
+
+
+    }catch(error){
+        document.getElementById(response.message).innerText('Data transfer failed, should see data from game');
+
+    }
+    //whatever we need for the game start is done here, like go to lobby screen and do backend stuff
+}
