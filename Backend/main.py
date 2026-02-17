c4da562052beb6199c624df2e3cf99b330620e86
Bryce Hart <bryce.hart23@gmail.com>
2026-02-17 14:12:14 -0500

    added more explanations of examples for users and notes on gameID

--- Backend/main.py
+++ Backend/main.py
@@ -11,16 +11,20 @@ class gameStates(Enum):
     IN_GAME = 2
     GAME_OVER = 3
     END_OF_GAME_RESULTS = 4
-    END_INSTANCE = 5
+    END_INSTANCE = 5 #end game instance and take it out of the list
 
 active_games = [] #store all games that are going on right now here
 
 
 #returns the size of active games, and that will be the id for the current game
+#this id will simply just be the spot in the vector that the id is at.
 def setGameId():
     return len(active_games)
 
 
+
+
+#Calling this creates a new game object.
 def createNewGame():
     return game()
 
@@ -43,23 +47,36 @@ class game:
         self.map = CM.generateMap()
 
 
-    def userJoined(self):
+    def userJoined(self, person : userf.user):
+        if(self.currentGameState != gameStates.IN_LOBBY):
+            pass
+            #tell the user that they cannot join, game is in progress
+        if(len(self.users) == self.MAX_PLAYERS):
+            pass
+            #tell the user they cant join max player limit has been reached, or just black out join button, or both!
         #this should not have logic related to the individual, rather just the user as it relates to this game instance
         #for example, base origin, and checking what player number they are in this list
-        pass
+        return
         
 
     def userLeft(self, usersId : str):
         self.users.remove(usersId)
+        return
+
+    def thisGameHasStarted(self):
+        self.currentGameState = gameStates.IN_GAME
+        return
         
 
 
 
 
 #user requested new game
-def pressedStartNewGame():
+def pressedCreateNewGame():
     currentGame = createNewGame()
     currentGame.userJoined(userf.createNewUser())
+
+    
 
 
 #server related functions go below here
